6c57cf50eb9305281764f477e3a95f1d31adbae6
Cristopher Arredondo <crxsx.0@gmail.com>
2025-08-25 21:41:20 -0400

    Eliminar espacios en blanco y mejorar la legibilidad del código en app.js

--- app.js
+++ app.js
@@ -1,49 +1,14 @@
 const amigosLista = [];
 
-
 function agregarAmigo(){
     let amigos = document.getElementById('amigo').value;
     console.log(amigos)
     amigosLista.push(amigos)
     limpiarCaja()
+    console.log(amigosLista)
     return;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function limpiarCaja(){
     document.getElementById('amigo').value = "";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
