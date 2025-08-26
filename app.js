fe928dd437162c5882b20eca23bf72fe8b8a906f
Pedro Hilario <pedroahilariom@gmail.com>
2025-08-25 20:26:10 -0500

    agregar git

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
+let amigos = [];
+
+
+function agregarAmigo(){
+    let amigos = document.getElementById('amigo').value;
+    console.log(amigos)
+    limpiarCaja()
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function limpiarCaja(){
+    document.getElementById('amigo').value = "";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
